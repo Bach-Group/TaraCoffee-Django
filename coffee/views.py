93c813186f79d0a7489a5de5c76e1f4b8e22b15b
thanh <bachvthanh95@gmail.com>
2022-03-11 10:22:09 +0700

    add image django

--- coffee/views.py
+++ coffee/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from .models import Menu, Config
 
 def index(request):
-    menu=Menu.objects.all()[:2]
+    menu=Menu.objects.all()[:8]
+    menu2=Menu.objects.all()[8:]
 #     qs = MyModel.objects.filter(blah = blah)
 # if qs.count() > 0:
 #     return qs[0]
 # else:
 #     return None
-    contex={"menu":menu}
+    contex={"menu":menu,"menu2":menu2}
     return render(request,"coffee/index.html", contex)
 
 # Create your views here.
